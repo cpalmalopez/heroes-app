f3f1d52c96a70f053e7cf7f0dd51f68f7f538d25
xclaudioh <xclaudioh>
2021-04-03 21:30:05 -0300

    rutas privadas

--- src/components/login/LoginScreen.js
+++ src/components/login/LoginScreen.js
@@ -1,21 +1,36 @@
-import React from 'react'
+import React, { useContext } from 'react';
+import { AuthContext } from '../../auth/AuthContext';
+import { types } from '../../types/types';
 
 export const LoginScreen = ({ history }) => {
 
+    const { dispatch } = useContext( AuthContext );
+
     const handleLogin = () => {
-        history.replace("/");
+
+        const lastPath = localStorage.getItem('lastPath') || '/';
+
+        dispatch({
+            type: types.login,
+            payload: {
+                name: 'Claudio'
+            }
+        });
+        history.replace( lastPath );
     }
 
     return (
         <div className="container mt-5">
             <h1>Login</h1>
-            <hr/>
+            <hr />
+
             <button
                 className="btn btn-primary"
                 onClick={ handleLogin }
             >
                 Login
             </button>
+
         </div>
     )
 }
--- src/routers/DashboardRoutes.js
+++ src/routers/DashboardRoutes.js
@@ -10,6 +10,7 @@ export const DashboardRoutes = () => {
     return (
         <>
             <Navbar />
+
             <div className="container mt-2">
                 <Switch>
                     <Route exact path="/marvel" component={ MarvelScreen } />
